a1c17ddfa285737245f1fa138b7e29cf922fdbff
Alexhong1269 <Alexhong1269@gmail.com>
2025-05-07 15:59:50 -0400

    adding more tools to the toolbar

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -1,6 +1,20 @@
 import { useCallback } from "react"
 import Quill from "quill"
+import "./styles.css"
 import "quill/dist/quill.snow.css"
+
+//creating more options to select for tool bar
+const TOOLBAR_OPTIONS = [
+    [{header: [1, 2, 3, 4, 5, 6,false] }],
+    [{font: [] }],
+    [{list: "ordered" }, {list: "bullet"}],
+    ["bold", "italic", "underline"],
+    [{color: []}, {background: [] }],
+    [{script: "sub"}, {script: "super"}],
+    [{align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+]
 
 export default function TextEditor() {
     const wrapperRef = useCallback((wrapper) => {
@@ -10,9 +24,9 @@ export default function TextEditor() {
         wrapper.innerHTML = " "
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, { theme: "snow"})
+        new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} })
 
     }, [])
     
-    return <div id = "container" ref = {wrapperRef}></div>
+    return <div className = "container" ref = {wrapperRef}></div>
 }
